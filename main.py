18b16a60c6ceb79ef438229e0e16d4aff7baa625
Alice Cooper <62759652+alicexcooperx@users.noreply.github.com>
2020-04-20 21:08:09 +0100

    Added the DTO object into main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import Parser
 # Wrapper Class - Just opens the JSON Ticket and hands it off to the parser.
 # Can be safely removed when integrating.
 
@@ -6,7 +7,10 @@ def main():
     ticket = open("ticket.json", "r")
     ticket_contents = ticket.read()
     ticket.close()
-    print(ticket_contents)
+    parsing = Parser.Parser()
+    DTOObject = parsing.parse_ticket(ticket_contents)
+
+    print(DTOObject.attachments)
 
 
 if __name__ == '__main__':
