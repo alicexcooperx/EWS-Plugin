cecaa6717b4e38a53585f547654151d1a95fc0ec
Alice Cooper <62759652+alicexcooperx@users.noreply.github.com>
2020-04-20 17:24:58 +0100

    Added main entry point to program.

---
+++ main.py
@@ -0,0 +1,10 @@
+# Wrapper Class - Just opens the JSON Ticket and hands it off to the parser.
+# Can be safely removed when integrating.
+
+
+def main():
+    print("Test code, to see if this is working.")
+
+
+if __name__ == '__main__':
+    main()
