d2ecc53841365ea73eeaa0fc0d712a4fb0d8514f
Alice Cooper <62759652+alicexcooperx@users.noreply.github.com>
2020-04-24 20:35:30 +0100

    aded creating directory for the ticket which holds cratorid and ticketid

--- JSONParser.py
+++ JSONParser.py
@@ -1,10 +1,12 @@
 import json
 import DTO
+import os
 
 
 class JSONParser:
 
-    def parse_ticket(self, ticket):
+    def parse_ticket(self, ticket, been_scanned=False, flagged_filename=None, flagged_hash=None):
+
         payload = json.loads(ticket)
 
         attribute_values = payload["attributeValues"]
@@ -18,6 +20,21 @@ class JSONParser:
         cyber_ticket_info = payload["cyberticketEmails"]
         attachments = payload["attachments"]
 
-        DTOObject = DTO.DTO(title, description, creator_id, organization_id, comments, references,
-                            local_organization_ticket_info, cyber_ticket_info, attribute_values, attachments)
-        return DTOObject
+        ticket_heading = payload["title"]
+        sub_directory = ticket_heading
+        ticket_creator = payload["creatorId"]
+        directory = ticket_creator
+
+        path = ("C:\\Users\\angel\\PycharmProjects\\EWS-Plugin\\" + directory + "\\" + sub_directory)
+        try:
+            os.makedirs(path)
+        except OSError:
+            print("Creation of directory %s failed" % path)
+        else:
+            print("Successfully created the directory %s" % path)
+
+        dtoObject = DTO.DTO(title, description, creator_id, organization_id, comments, references,
+                            local_organization_ticket_info, cyber_ticket_info, attribute_values, attachments,
+                            flagged_filename, flagged_hash, been_scanned)
+        return dtoObject
+
