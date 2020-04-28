ef6db6959039d9479ff6719517ca484c86b761f4
Alice Cooper <62759652+alicexcooperx@users.noreply.github.com>
2020-04-28 21:50:59 +0100

    added it so it feeds in the attachments instead of hard coded file

--- sanitizationVT.py
+++ sanitizationVT.py
@@ -1,18 +1,17 @@
-import os
 import requests
 import json
 
 
 class SanitizationAV:
 
-    def sanitiseAV(self, attachments):
+    def sanitiseAV(self, dto_object):
 
         url = 'https://www.virustotal.com/vtapi/v2/file/scan'
         api_key = '050e4c7768f9d896b852a271ed0718b0a87a24a7907a121337112dfcb3134bcd'
-        files = [{'file': ('C:\\Users\\angel\\PycharmProjects\\EWS-Plugin\\attachments\\htmltest.html',
-                           open('C:\\Users\\angel\\PycharmProjects\\EWS-Plugin\\attachments\\htmltest.html', 'rb'))},
-                 {'file': ('C:\\Users\\angel\\PycharmProjects\\EWS-Plugin\\attachments\\testtext2.txt',
-                           open('C:\\Users\\angel\\PycharmProjects\\EWS-Plugin\\attachments\\testtext2.txt', 'rb'))}]
+        files = []
+
+        for file in dto_object.attachments:
+            files.append({'file': (file, open(file, 'rb'))})
 
         for file in files:
             response = requests.post(url, files=file, params={"apikey": api_key})
@@ -22,6 +21,7 @@ class SanitizationAV:
 
             for item in output.items():
                 if item[0] == 'sha256':
+                    dto_object.attachments_hash.append(item[1])
                     print("{0}: {1}".format(item[0], item[1]))
                     sha256_file = item[1]
 
@@ -30,8 +30,10 @@ class SanitizationAV:
 
             for index, value in report_resp.items():
                 if index == "positives" and value == 0:
+                    dto_object.attachments_virus.append(0)
                     print("No Viruses Detected")
                 elif index == "positives" and value > 0:
+                    dto_object.attachments_virus.append(index['positives'])
                     print("{0} positive results found".format(index['positives']))
 
 # For each file in the directory,
