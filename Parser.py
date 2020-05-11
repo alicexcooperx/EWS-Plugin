20996a717ab27882f01aaec0130331de6659949c
Alice Cooper <62759652+alicexcooperx@users.noreply.github.com>
2020-05-11 14:35:28 +0100

    Added docstrings for parser

--- Parser.py
+++ Parser.py
@@ -5,7 +5,19 @@ class Parser:
     json_parser = ""
 
     def __init__(self):
+        """
+        This is the main class which
+
+        | @param self: access the attributes of the class
+        """
         self.json_parser = JSONParser.JSONParser()
 
     def parse_ticket(self, ticket):
+        """
+        This is the main method of the class which is used as a wrapper class for the program.
+
+        | @param self: access the attributes of the class
+        | @param ticket: access the json ticket
+        | @return: The json ticket
+        """
         return self.json_parser.parse_ticket(ticket)
