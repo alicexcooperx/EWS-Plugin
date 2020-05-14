3b80b301d4dd6c0b69af6172b02a1c5b8e5bc3e1
Alice Cooper <62759652+alicexcooperx@users.noreply.github.com>
2020-05-14 15:40:34 +0100

    Added the redaction in main so program can be run updated the test ticket with relevant information and also changed the redaction so that it isnt stuck in infinite loop

--- Redaction.py
+++ Redaction.py
@@ -5,6 +5,16 @@ import glob
 
 class Redaction:
 
+    def __init__(self):
+        self.date_time_obj = datetime.now()
+        self.time_string = self.date_time_obj.strftime("%d-%b-%Y %H-%M-%S")
+        self.directory = self.time_string
+        self.parent_dir = r"C:\\Users\\angel\\OneDrive\\Documents\\Diss\\"
+        self.full_directory_path = self.parent_dir + self.directory
+        self.redactionbulk()
+        self.removefiles(self.full_directory_path)
+
+
     def redactionbulk(self):
         """
         Creates a directory of files which is based on the current date and time
@@ -12,29 +22,58 @@ class Redaction:
 
         | @param self: access the attributes of the class
         """
-        date_time_obj = datetime.now()
-        time_string = date_time_obj.strftime("%d-%b-%Y %H-%M-%S")
-        directory = time_string
-        parent_dir = r"C:\\Users\\angel\\OneDrive\\Documents\\Diss\\"
-        full_directory_path = parent_dir + directory
-        command = 'bulk_extractor -o "' + full_directory_path + '" -R "C:\\Users\\angel\\PycharmProjects\\EWS-Plugin' \
+
+        command = 'bulk_extractor -o "' + self.full_directory_path + '" -R "C:\\Users\\angel\\PycharmProjects\\EWS-Plugin' \
                                                                 '\\attachments" '
         os.system(command)
 
-    def removefiles(self, fullDirectoryPath):
+    def removefiles(self, full_directory_path):
         """
         Removes all of the files which have 0KB in them so that the program can ignore irrelevant files.
+        It also removes the
 
         | @param self: access the attributes of the class
         | @param fullDirectoryPath: Passes the full directory path
         """
-        in_dir = fullDirectoryPath
+        in_dir = full_directory_path
         os.chdir(in_dir)
         file_list = glob.glob("*.txt")
 
         for filename in file_list:
+            # Removes all files which are 0KB since they have no information in them.
             if os.stat(filename).st_size == 0:
                 os.remove(filename)
+
+        for file_name in os.listdir(full_directory_path):
+            try:
+                string_parts = []
+                # All histogram files are removed from the directory.
+                str_file_name = str(file_name)
+
+                string_parts.append(str_file_name.split("_"))
+                for i in string_parts:
+                    if "histogram.txt" in i:
+                        os.remove(file_name)
+            except IndexError:
+                continue
+
+        for file_name_services in os.listdir(full_directory_path):
+            try:
+                string_parts = []
+                # All services files are removed from the directory.
+                str_file_name = str(file_name_services)
+
+                string_parts.append(str_file_name.split("_"))
+                for i in string_parts:
+                    if "services.txt" in i:
+                        os.remove(file_name_services)
+            except IndexError:
+                continue
+
+        # if os.path.exists(full_directory_path + "report.xml"):
+            # os.remove("report.xml")
+        # else:
+            # print("The file does not exist")
 
     def valueContains(self, redactArray, startPos, increment):
         """
@@ -44,7 +83,9 @@ class Redaction:
         | @param redactArray: The array of information that has been found that needs to be redacted
         | @param startPos: The start position of the byte of sensitive information
         | @param increment: The increment of the byte
+        | @param fullDirectoryPath: Passes the full directory path
         """
+
         test_result = startPos
         for x in range(increment):
             if test_result in redactArray:
@@ -52,54 +93,3 @@ class Redaction:
             test_result += 1
         return False
 
-    with open('attachmentsoutput\\domain.txt', 'r') as f:
-
-        redaction = {}
-
-        for line in f:
-            try:
-                redact_file = os.path.normpath(line.split('\t')[0].split('ô€€œ-')[0])
-                redact_location = line.split('\t')[0].split('ô€€œ-')[1]
-                redact_string = line.split('\t')[1]
-
-                tempLocation = int(redact_location)
-
-                for letter in redact_string:
-                    fileNameKey = redaction.get(redact_file)
-                    if not fileNameKey:
-                        temp = [tempLocation]
-                        redaction[redact_file] = temp
-                        tempLocation += 1
-                    else:
-                        fileNameKey.append(tempLocation)
-                        tempLocation += 1
-
-            except IndexError:
-                continue
-
-        keys = redaction.keys()
-        for key in keys:
-            bytesToRedact = redaction.get(key)
-            byteCount = 1024
-            byte = 0
-            inFile = open(key, "rb")
-            outFile = open((key + ".redacted"), "wb")
-
-            while True:
-                buffer = inFile.read(byteCount)
-
-                if buffer == b"":
-                    break
-
-                if valueContains(bytesToRedact, byte, byteCount):
-                    for x in range(byteCount):
-                        currentByte = byte + x
-
-                        if currentByte in bytesToRedact:
-                            outFile.write(b'*')
-                        else:
-                            outFile.write(buffer[x:(x+1)])
-
-                else:
-                    outFile.write(buffer)
-                byte += byteCount
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import Parser
 import sanitizationVT
-
+import Redaction
+import os
 
 def main():
     """
@@ -15,6 +16,66 @@ def main():
     dto_object = parsing.parse_ticket(ticket_contents)
     sanitise = sanitizationVT.SanitizationAV()
     sanitise.sanitiseAV(dto_object)
+    rdc = Redaction.Redaction()
+    redaction = {}
+
+    for filen in os.listdir(rdc.full_directory_path):
+        if filen == "report.xml" or filen == "json.txt":
+            continue
+        with open(os.path.join(os.getcwd(), filen), 'r') as f:
+            for line in f:
+                # Gets values for file, location and the string to redact
+                try:
+                    redact_file = os.path.normpath(line.split('\t')[0].split('ô€€œ-')[0])
+                    redact_location = line.split('\t')[0].split('ô€€œ-')[1]
+                    redact_string = line.split('\t')[1]
+
+                    tempLocation = int(redact_location)
+
+                    # Gets the letter and make a temporary location if it doesnt exist.
+                    for letter in redact_string:
+                        if redact_file not in redaction:
+                            temp = [tempLocation]
+                            redaction[redact_file] = temp
+                            tempLocation += 1
+                        else:
+                            fileNameKey = redaction.get(redact_file)
+                            fileNameKey.append(tempLocation)
+                            tempLocation += 1
+
+                except IndexError:
+                    continue
+
+    keys = redaction.keys()
+    for key in keys:
+        # Want to make sure that the system isn't over-using memory so it has been limited.
+        bytesToRedact = redaction.get(key)
+        bytesToRedact.sort()
+        byteCount = 1024
+        byte = 0
+        inFile = open(key, "rb")
+        outFile = open((key + ".redacted"), "wb")
+
+        while True:
+            buffer = inFile.read(byteCount)
+
+            if buffer == b"":
+                break
+
+            if rdc.valueContains(bytesToRedact, byte, byteCount):
+                for x in range(byteCount):
+                    currentByte = byte + x
+
+                    if currentByte in bytesToRedact:
+                        outFile.write(b'*')
+                    else:
+                        outFile.write(buffer[x:(x + 1)])
+
+            else:
+                outFile.write(buffer)
+            byte += byteCount
+
+
 
 if __name__ == '__main__':
     main()
